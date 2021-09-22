3a50176e93aa615bc1369f56524c95e3a132f0e1
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-09-22 02:43:18 +0200

    speed

--- src/lib/movement/others.js
+++ src/lib/movement/others.js
@@ -1,6 +1,6 @@
 const moveOthers = models => {
   if (models[0]) {
-    models[0].position.z += -5;
+    models[0].position.z -= 20;
     if (models[0].position.z < -7000) models[0].position.z = 8000;
   }
 };
